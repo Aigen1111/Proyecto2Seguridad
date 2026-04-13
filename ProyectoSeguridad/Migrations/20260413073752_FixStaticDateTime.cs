30de918a2bae37b6f8cc4eb443b1db56c8d42bc2
Aigen1111 <Carlos940794@gmail.com>
2026-04-13 01:47:45 -0600

    Agregar migración para DateTime estático

---
+++ ProyectoSeguridad/Migrations/20260413073752_FixStaticDateTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoSeguridad.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStaticDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaCreacion",
+                value: new DateTime(2026, 4, 13, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaCreacion",
+                value: new DateTime(2026, 4, 13, 7, 1, 51, 928, DateTimeKind.Utc).AddTicks(2841));
+        }
+    }
+}
